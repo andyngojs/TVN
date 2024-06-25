374740f7e96b5e3ab52f9f631036788799caa56b
dongngo <dongnbas@gmail.com>
2024-06-25 15:41:32 +0700

    style: fix some style css and change theme, color

--- src/app/[...not_found]/page.js
+++ src/app/[...not_found]/page.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function NotFoundCatchAll() {
   return (
     <Layout>
-      <div className="flex flex-col justify-center items-center bg-white py-10">
+      <div className="flex flex-col justify-center items-center py-10">
         <Image
           src={"/icons/under_construction.svg"}
           alt="This page is constructing"
@@ -12,7 +12,7 @@ export default function NotFoundCatchAll() {
           height={500}
         />
 
-        <h2 className="text-xl font-bold mt-10 mb-12">
+        <h2 className="text-xl font-bold mt-10 mb-12 text-white">
           Trang này đang trong quá trình phát triển. Chúng tôi sẽ sớm hoàn thiện
           và cập nhật đầy đủ nội dung. Xin lỗi vì sự bất tiện này!
         </h2>
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,44 +1,60 @@
-import clsx from 'clsx';
-import Header from './Header';
-import config from '../../app.config.json';
-import styles from '@/styles/components/layout.module.scss';
-import {HEADER_HEIGHT} from './Header';
+import clsx from "clsx";
+import Header from "./Header";
+import config from "../../app.config.json";
+import styles from "@/styles/components/layout.module.scss";
+import { HEADER_HEIGHT } from "./Header";
 
 const CONTACT_FOOTER_DATA = [
   {
-    title: 'Điện thoại',
+    title: "Điện thoại",
     value: config.hotline,
   },
   {
-    title: 'Email',
+    title: "Email",
     value: config.email,
   },
   {
-    title: 'VP Đại diện',
+    title: "VP Đại diện",
     value: config.address,
   },
 ];
 
 const INFO_COMPANY_DATA = [
   {
-    title: 'Mã số thuế',
+    title: "Mã số thuế",
     value: config.masothue,
   },
   {
-    title: 'Ngày thành lập',
+    title: "Ngày thành lập",
     value: config.foundedTime,
   },
   {
-    title: 'Trụ sở',
+    title: "Trụ sở",
     value: config.headquarter,
   },
 ];
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
+  const renderServiceList = () => {
+    return config.serviceList.map((item, index) => {
+      return (
+        <p
+          key={index}
+          className={clsx(styles.navFooterItem, styles.navFooterContactItem)}
+        >
+          {item}
+        </p>
+      );
+    });
+  };
+
   const renderContactFooter = (contactData = CONTACT_FOOTER_DATA) => {
     return contactData.map((item, index) => {
       return (
-        <p key={index} className={clsx(styles.navFooterItem, styles.navFooterContactItem)}>
+        <p
+          key={index}
+          className={clsx(styles.navFooterItem, styles.navFooterContactItem)}
+        >
           {item.title}: {item.value}
         </p>
       );
@@ -48,7 +64,10 @@ export default function Layout({children}) {
   const renderInfoCompany = (infoData = INFO_COMPANY_DATA) => {
     return infoData.map((item, index) => {
       return (
-        <p key={index} className={clsx(styles.navFooterItem, styles.navFooterContactItem)}>
+        <p
+          key={index}
+          className={clsx(styles.navFooterItem, styles.navFooterContactItem)}
+        >
           {item.title}: {item.value}
         </p>
       );
@@ -57,23 +76,25 @@ export default function Layout({children}) {
 
   return (
     <div
-      className={clsx('flex flex-col justify-between relative min-h-[100vh]')}>
+      className={clsx("flex flex-col justify-between relative min-h-[100vh]")}
+    >
       <Header />
 
-      <main style={{marginTop: HEADER_HEIGHT}}>{children}</main>
+      <main style={{ marginTop: HEADER_HEIGHT }}>{children}</main>
 
-      <footer className="w-full bg-neutral-900">
+      <footer className="w-full bg-neutral-950">
         <div
           className={clsx(
-            'flex flex-col lg:flex-row',
-            styles.footerMainContainer,
-          )}>
-          <div className={clsx('', styles.footerBlock)}>
+            "flex flex-col lg:flex-row",
+            styles.footerMainContainer
+          )}
+        >
+          <div className={clsx("", styles.footerBlock)}>
             <div>
               <img
                 src={config.logo}
                 alt="logo-tvn"
-                className={clsx('w-[130px] h-auto')}
+                className={clsx("w-[130px] h-auto")}
               />
             </div>
             <div className={clsx(styles.blockContent)}>
@@ -81,13 +102,16 @@ export default function Layout({children}) {
             </div>
           </div>
 
-          <div className={clsx('', styles.footerBlock)}>
+          <div className={clsx("", styles.footerBlock)}>
             <div className={clsx(styles.blockTitle)}>Dịch vụ</div>
 
+            <div className={clsx(styles.blockContent)}>
+              {renderServiceList()}
+            </div>
           </div>
 
-          <div className={clsx('', styles.footerBlock)}>
-            <div className={clsx('uppercase', styles.blockTitle)}>
+          <div className={clsx("", styles.footerBlock)}>
+            <div className={clsx("uppercase", styles.blockTitle)}>
               {config.fullNameCompany}
             </div>
             <div className={clsx(styles.blockContent)}>
@@ -97,11 +121,10 @@ export default function Layout({children}) {
         </div>
 
         <div
-          className={clsx(
-            'flex items-center justify-center bg-black h-[50px]',
-          )}>
-          <p className={clsx('text-sm text-gray-200')}>
-            Copyright © 2024 - TVN. All Rights Reserved.
+          className={clsx("flex items-center justify-center bg-black h-[50px]")}
+        >
+          <p className={clsx("text-sm text-gray-200")}>
+            Copyright © 2024 - Sắc Việt Entertainment. All Rights Reserved.
           </p>
         </div>
       </footer>
